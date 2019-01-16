c32df0675a83ce65fcd8043ef1084a59ec77e4fb
daiki-darker <janne2570darker@gmail.com>
2019-01-16 18:24:08 +0900

    Boltレスポンシブのレビューをお願いします！！

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
-  $(".header__ham").click(function(){
-    $(this).toggleClass(active);
-    $(this).next().toggleClass("onanimation");
-    $("btn__contact").hide();
-    $("btn__contact").each(function(i){
-      $(this).delay(80 * i).fadeIn(500);
-    }
-    }
-  }
-}
+// (function() {
+//   (".header__ham").click(function(){
+//     (this).toggleClass(active);
+//     (this).next().toggleClass("onanimation");
+//     (".btn__contact__sp").hide();
+//     (".btn__contact__sp").each(function(i){
+//       (this).delay(80 * i).fadeIn(500);
+//     });
+//   });
+// });
+
+// (function() {
+//   (".header__ham").click(function(){
+//     (this).toggleClass(active);
+//     (this).next().toggleClass("onanimation");
+//     (".btn__contact__sp").hide();
+//     (".btn__contact__sp").each(function(i){
+//       (this).delay(80 * i).fadeIn(500);
+//     });
+//   });
+// });
+function(){
+  (".header__ham").click(function(){
+  console.log("hello world");
+});
+};
